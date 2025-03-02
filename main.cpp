f01fe0bf0b18d4dff206055454fa52701190d5dc
Xuanli Lin <kaz@kazumi.blog>
2025-03-02 16:42:39 -0700

    reverted X change

--- main.cpp
+++ main.cpp
@@ -240,7 +240,7 @@ int main(int argc, char **argv) {
     std::vector<std::vector<int>> best_results;
 
     // Iterate through the possible X values to determine which one gives the best outcome
-    for (int x = 13; x <= 13; x++) {
+    for (int x = 0; x <= X_ub; x++) {
         X = x;
         // Update timing info after x changes
         for (int n = 1; n <= N; n++) {
