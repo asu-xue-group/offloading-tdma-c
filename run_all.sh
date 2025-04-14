f8aa53092d5db8f3deb9b4e83799f4c0884a5536
Xuanli Lin <kazumi@asu.edu>
2025-04-14 14:33:22 -0700

    updated the test case for delay

--- run_all.sh
+++ run_all.sh
@@ -2,10 +2,10 @@
 
 # Iterate through the test files
 
-for size in "small" "medium" "big"; do
+for size in "small" "medium" "big" "bigger"; do
   for num in $(seq 1 10); do
     for mode in -1 0 1; do
-        ./tdma_knapsack "test_cases/${size}/${size}_${num}/input.json" $mode
+        ./tdma_knapsack "test_cases/${size}/${size}_${num}/input.json" $mode 1
     done
   done
 done
