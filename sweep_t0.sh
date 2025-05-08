540d7729810fe6e1acd0e74ba6d547ba5599af57
Xuanli Lin <kazumi@asu.edu>
2025-05-08 16:48:28 -0700

    deleted unnecessary shell script

--- sweep_t0.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Iterate through the test files
-
-for i in {2..10}; do
-  for flag in -1 1; do
-    ./tdma_knapsack "test_files/med-large/$i/input.txt" $flag 10 25
-  done
-done
